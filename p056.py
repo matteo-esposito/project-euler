31796f640b995ecdee397a7f9095d83dd5032dc2
Matteo Esposito <actumatteo@gmail.com>
2019-05-02 21:43:03 -0400

    p052 + folder mgmt

---
+++ p056.py
@@ -0,0 +1,26 @@
+# Project Euler
+# Problem 56: Powerful digit sum
+import pe_functions as pe
+
+def sol():
+    '''
+    Solution to project euler problem.
+    '''
+    val = 0
+    val_list = []
+    sums = []
+
+    for a in range(50, 100):
+        for b in range(50, 100):
+            val_list.append(a**b)
+
+    for n in val_list:
+        temp_val = 0
+        for d in str(n):
+            temp_val += int(d)
+        sums.append(temp_val)
+            
+    return max(sums)
+
+if __name__ == '__main__':
+    pe.runSolution(sol)
